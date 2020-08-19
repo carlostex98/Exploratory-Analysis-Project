8aafa1d51b4b94997bfeb718c9e6067e14d29dd9
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-08-19 09:40:38 -0600

    Fixing minor things

--- fifthplot.R
+++ fifthplot.R
@@ -1,15 +1,18 @@
-NEI <- readRDS("summarySCC_PM25.rds")
+NEI_data <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+mrg_data <- merge(NEI_data, SCC, by = "SCC")
 
+library(ggplot2)
 
-##building plot
-year <- c(1999, 2002, 2005, 2008)
-total_pollution <- c(sum(NEI[NEI$year=="1999" & NEI$fips==24510,6]), 
-                     sum(NEI[NEI$year=="2002" & NEI$fips==24510,6]), 
-                     sum(NEI[NEI$year=="2005" & NEI$fips==24510,6]), 
-                     sum(NEI[NEI$year=="2008" & NEI$fips==24510,6]))
+subNEI <- NEI_data[NEI_data$fips=="24510" & NEI_data$type=="ON-ROAD",  ]
 
-plot(year, total_pollution, type = "l", xlab = "year", ylab = "total pollution")
-dev.copy(png, file = "plot5.png")
+totalg <- aggregate(Emissions ~ year, subNEI, sum)
+
+png("plot5.png", width=900, height=500)
+g <- ggplot(totalg, aes(factor(year), Emissions/1000))
+g <- g + geom_bar(stat = "identity") + xlab("years") +
+  ylab(expression('Total PM Emissions-kilotons-')) +
+  ggtitle(expression('Total PM Emissions from motor vehicle sources from 1999-2008 in Baltimore City'))
+print(g)
 dev.off()
